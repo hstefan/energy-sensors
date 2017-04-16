22700a6fb2d85838c46204ec8b655276d13f2268
Hugo Puhlmann <hugopuhlmann@gmail.com>
2017-04-16 12:21:50 -0300

    Adding setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='EnergySensors',
+      version='0.0.1',
+      description='A collection of services meant to receive, parse, and process telemetric data'
+                  'from power sensors.',
+      author='Hugo Puhlmann',
+      author_email='hugopuhlmann@gmail.com',
+      url='https://github.com/hstefan/energy-sensors',
+      packages=['energy_sensors'])
